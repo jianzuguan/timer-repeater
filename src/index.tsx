29c6e9c627204f2c241950d1d196c55093ba10cd
Jianzu Guan <jianzuguan@luminalearning.com>
2020-12-13 20:54:47 +0000

    restructre files

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import Title from "@timerrepeater/Title";
+
+function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!1</Text>
+      <Title />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default App;
